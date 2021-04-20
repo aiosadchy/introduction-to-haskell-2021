04c30f48722bd9daecdb705e041379ca087247b3
Alexander Osadchy <a.i.osadchy@gmail.com>
2021-04-20 21:39:08 +0300

    Fix predicate

--- exam/lines.hs
+++ exam/lines.hs
@@ -1,9 +1,10 @@
 get_repeating_lines :: [String] -> [String]
 get_repeating_lines [] = []
-get_repeating_lines (l:ls) | elem l ls = (l:(get_repeating_lines rest))
-                           | otherwise = get_repeating_lines ls
-                           where rest = filter p ls
-                                 p a  = a /= l
+get_repeating_lines (l:ls) | length (filter p ls) > 1 = (l:(get_repeating_lines rest))
+                           | otherwise                = get_repeating_lines ls
+                           where rest = filter n ls
+                                 n a  = a /= l
+                                 p a  = a == l
 
 
 main = interact (unlines . get_repeating_lines . lines)
